37a668ccd5051e814d988e45dfcb9e9da861e137
Nate Brady <nate@ultimus.cloud>
2023-06-16 18:27:59 -0500

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,17 +2,17 @@
 #Entrypoint script for enterprise connector installer
 
 #Check that necessary envs are set
-if [[ -z "${SKYHIGH_USERNAME}"] ]; then
+if [ -z "${SKYHIGH_USERNAME}"]; then
     echo "Missing Skyhigh username.  Please set the environment variable SKYHIGH_USERNAME."
     exit 1
 fi
 
-if [[ -z "${SKYHIGH_PASSWORD}"] ]; then
+if [ -z "${SKYHIGH_PASSWORD}"]; then
     echo "Missing Skyhigh password.  Please set the environment variable SKYHIGH_PASSWORD."
     exit 1
 fi
 
-if [[ -z "${SKYHIGH_SALT}"] ]; then
+if [ -z "${SKYHIGH_SALT}"]; then
     echo "Missing Skyhigh salt.  Please set the environment variable SKYHIGH_SALT."
     exit 1
 fi
